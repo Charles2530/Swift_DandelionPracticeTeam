e5a5be80324a8dd14718e2c151a98764c5933fd3
Charles <2569337619@qq.com>
2023-09-29 17:04:38 +0800

    Initial Commit

---
+++ DandelionPracticeTeam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DandelionPracticeTeam
+//
+//  Created by Charles on 2023/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DandelionPracticeTeam/DandelionPracticeTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandelionPracticeTeamApp.swift
+//  DandelionPracticeTeam
+//
+//  Created by Charles on 2023/9/29.
+//
+
+import SwiftUI
+
+@main
+struct DandelionPracticeTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
